6f297f97224d16415628e07f40a1f47e24a9c37c
Arvind <thakurarvindkr10@gmail.com>
2023-06-05 14:38:31 +0530

    added reviews star

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./ProductCard.css";
-import formatCurrency from 'format-currency'
+import Rating from "./Rating";
 
 const ProductCard = ({ product }) => {
   return (
     <div className="productCard__wrapper">
       <div>
-          <img className="productCard__img" src={product.image} alt="" /> 
-          </div>
-          <h4>{product.name}</h4>
-          <div className="productCard__price">
-              <h5>$ {product.price}</h5>
-          </div>
+        <img className="productCard__img" src={product.image} alt="" />
+      </div>
+      <h4>{product.name}</h4>
+      <div className="productCard__price">
+        <h5>$ {product.price}</h5>
+      </div>
+      <div className="ProductCard__Rating">
+        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+      </div>
+      <button className="ProductCard__button">add to basket</button>
     </div>
   );
 };
